2444cdd85c48ad253461d1e232641e0efd1ad4b2
Sreeraj Dabbiru <sreerajscs916225@gmail.com>
2025-09-09 19:16:53 +0530

    ui polish

--- sih/index/urls.py
+++ sih/index/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
+from index import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("index.urls")),
+    path("", views.home, name="home"),
 ]
