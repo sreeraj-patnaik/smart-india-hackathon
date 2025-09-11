fd754ec5a910ae9686973d73a366dd644cfd39e2
Dabbiru Sreeraj <sreerajscs916225@gmail.com>
2025-09-11 12:10:26 +0530

    Update admin.py

--- sih/index/admin.py
+++ sih/index/admin.py
@@ -4,3 +4,15 @@ from django.contrib import admin
 
 from .models import Profile
 admin.site.register(Profile)
+
+from .models import DoctorContact
+admin.site.register(DoctorContact)
+
+from .models import TestReport
+admin.site.register(TestReport)
+
+from .models import ChatMessage
+admin.site.register(ChatMessage)
+
+from .models import ForumPost
+admin.site.register(ForumPost)
